1dd0c7714de72d7f3e6ac0c3f2d356472c427f9b
HIMARS-M142 <maksympona@gmail.com>
2023-09-05 14:57:24 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -4,12 +4,14 @@ export const FriendList = ({ friends }) => {
   return (
     <FriendListUl className="friend-list">
       {friends.map(({ id, isOnline, avatar, name }) => {
-        <FriendListItem
-          id={id}
-          idOnline={isOnline}
-          avatar={avatar}
-          name={name}
-        />;
+        return (
+          <FriendListItem
+            key={id}
+            isOnline={isOnline}
+            avatar={avatar}
+            name={name}
+          />
+        );
       })}
     </FriendListUl>
   );
