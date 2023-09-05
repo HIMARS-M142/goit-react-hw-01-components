ed103a7701d5be520be0b972f052bbcbf99e2171
HIMARS-M142 <maksympona@gmail.com>
2023-09-05 15:00:46 +0200

    Update FriendListItem.jsx

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,7 +1,7 @@
 import { FriendListLi, FriendStatus } from './FriendListStyled';
-export const FriendListItem = ({ id, name, avatar, isOnline }) => {
+export const FriendListItem = ({ name, avatar, isOnline }) => {
   return (
-    <FriendListLi key={id} className="item">
+    <FriendListLi className="item">
       <FriendStatus $isOnline={isOnline} className="status"></FriendStatus>
       <img className="avatar" src={avatar} alt="User avatar" width="48" />
       <p className="name">{name}</p>
